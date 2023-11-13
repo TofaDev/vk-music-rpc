29149ae707c5e26509eb487e642dd712b88dfeb6
TofaDev <tofadevelop@gmail.com>
2023-11-13 13:41:08 +0700

    added music source

--- src/interface/ISong.ts
+++ src/interface/ISong.ts
@@ -1,4 +1,7 @@
+import { MusicSource } from "../enum/MusicSource";
+
 export interface ISong {
-    artist: string
-    songName: string
+  artist: string;
+  songName: string;
+  source: MusicSource;
 }
