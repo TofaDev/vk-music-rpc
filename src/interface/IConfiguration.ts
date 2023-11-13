e1bb70ed4e74dfe8d04097f0bbb65e01c108eb7f
TofaDev <tofadevelop@gmail.com>
2023-11-13 13:40:51 +0700

    updated app configuartion

--- src/interface/IConfiguration.ts
+++ src/interface/IConfiguration.ts
@@ -1,27 +1,32 @@
 export interface IDiscordButtons {
-    label: string
-    url: string
+  label: string;
+  url: string;
 }
 
 export interface IConfiguration {
-    debug: boolean | null
-    discordButtons: IDiscordButtons[]
-    websocketPort: number,
-    clientId: string
+  debug: boolean | null;
+  discordButtons: IDiscordButtons[];
+  websocketPort: number;
+  clientId: string;
 
-    activity: {
-        images: {
-            largeImageKey: string
-            smallImageKey: string
-        }
-        text: {
-            firstLine: string
-            secondLine: string
+  activity: {
+    images: {
+      largeImageKey: string;
+      smallImageKey: string;
+    };
+    text: {
+      firstLine: string;
+      secondLine: string;
 
-            nowPlaying: string
+      nowPlaying: string;
 
-            largeImageHint: string
-            smallImageHint: string
-        }
-    }
+      largeImageHint: string;
+      smallImageHint: string;
+    };
+  };
+
+  musicSource: {
+    yandex: string;
+    vk: string;
+  };
 }
